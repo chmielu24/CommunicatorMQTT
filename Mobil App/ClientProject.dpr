b34a7223baeac3b31b4f03befedf43d21b0e4cf6
chmielu24 <dawids42@gmail.com>
2018-03-18 20:36:55 +0100

    Create Client project

---
+++ Mobil App/ClientProject.dpr
@@ -0,0 +1,14 @@
+program ClientProject;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Client in 'Client.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
